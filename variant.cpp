16622c104542aae666922e9c07b40c17105b727a
Michael Birtman <michaelbirtman@Michaels-Mac-mini.local>
2025-04-27 16:35:29 +0200

    fixed test scripts

--- variant.cpp
+++ variant.cpp
@@ -12,7 +12,44 @@ Variant<int>
  Variant<int, bool, float>
 
  etc
-
- 
-
 */
+
+#include <cassert>
+#include <string>
+
+int main()
+{
+    Variant<int, std::string> v { 5 };
+
+    try
+    {
+        v.get<std::string>();
+        assert(false);
+    }
+    catch (std::bad_cast& e)
+    {
+        assert(true);
+    }
+    
+    assert( v.get<int>() == 5 );
+
+    std::string str { "Hello World" };
+    v = str;
+
+    assert( v.get<std::string>() == str );
+
+    v.get<std::string>().push_back('!');
+
+    assert( v.get<std::string>() == "Hello World!" );
+
+    v = 0;
+
+    assert( v.get<int>() == 0 );
+
+    assert( v.get<int>()++ == 0 );
+    assert( ++v.get<int>() == 2 );
+
+    v = str;
+
+    assert( v.get<std::string>() == str );
+}
