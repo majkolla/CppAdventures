7db344564fcb9c23ecc2815bc61b8be4a26b2ec6
michael birtman <micbi949@student.liu.se>
2025-04-30 20:53:49 +0200

    added a constructor and destructor

--- Deque/deque.hpp
+++ Deque/deque.hpp
@@ -8,7 +8,6 @@ methods:
 - pop_back
 operator[]
 - at
-test
 
 */
 
@@ -18,23 +17,36 @@ class deque
 {
 
 public: 
+    deque(size_t chunk_sz = 64, int initial_chunks = 4) 
+    : size_ {0}, chunks_ {initial_chunks}, chunk_size_ {chunk_sz}
+    {
+        data_ = new T*[chunks_]{}; // create an array of T pointers with chunks_ amount of pointer
+        for (size_t i = 0; i < chunks_; ++i)
+        {
+            data_[i] = new T[chunk_size_];
+        }
+        begin_ = 0; 
+    } 
+
+    ~deque()
+    {
+        for (size_t i = 0; i < chunks_; ++i)
+        {
+            delete[] data_;
+        }
+    }
+
+    void push_front()
+    {
+        
+    }
 
 
 
 private: 
-    int** begin{};
-    size_t size; // number of elements stored 
-    size_t chunks; // number of active chunks 
-    T** data; // a pointer to a 
-    size_t chunk_size; // the size for all chunks
-
-    int initial_chunk_count = 4; // intial size 
-
-    data_arr = new T*[initial_chunk_count]; // array of pointers to datatype T 
-
-    for (size_t i = 0; i < initial_chunk_count; ++i )
-    {
-        data[i] = new T[chunk_size];
-    }
-
+    T* begin_{}; // the index to the first value in the first chunk 
+    size_t size_; // number of elements stored 
+    size_t chunks_; // number of active chunks
+    T** data_;// a pointer to an array of pointers 
+    size_t chunk_size_; // the size for every chunk
 };
