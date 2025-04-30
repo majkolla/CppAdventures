c3c1071cc8e98774f6d39d47ce0d8b90a3dc1260
michael birtman <micbi949@student.liu.se>
2025-04-30 14:10:56 +0200

    added a lil

--- Deque/deque.hpp
+++ Deque/deque.hpp
@@ -17,4 +17,24 @@ template<typename T>
 class deque
 {
 
+public: 
+
+
+
+private: 
+    int** begin{};
+    size_t size; // number of elements stored 
+    size_t chunks; // number of active chunks 
+    T** data; // a pointer to a 
+    size_t chunk_size; // the size for all chunks
+
+    int initial_chunk_count = 4; // intial size 
+
+    data_arr = new T*[initial_chunk_count]; // array of pointers to datatype T 
+
+    for (size_t i = 0; i < initial_chunk_count; ++i )
+    {
+        data[i] = new T[chunk_size];
+    }
+
 };
